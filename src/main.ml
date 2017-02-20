ff8a22ce2004e72f0289356c821f040e7684c228
boondaburrah <boondaburrah@ackawi>
2017-02-20 16:48:54 -0500

    started adding opengl bits

--- src/main.ml
+++ src/main.ml
@@ -1,4 +1,5 @@
 open Tsdl
+open Tgl3
 open Result
 
 let () = match Sdl.init Sdl.Init.video with
@@ -7,6 +8,9 @@ let () = match Sdl.init Sdl.Init.video with
 	match Sdl.create_window ~w:640 ~h:480 "SDL OpenGL" Sdl.Window.opengl with
 	| Error (`Msg e) -> Sdl.log "error: %s" e; exit 1
 	| Ok w -> 
+	  Sdl.gl_set_attribute Sdl.Gl.context_profile_mask Sdl.Gl.context_profile_core;
+	  Sdl.gl_set_attribute Sdl.Gl.context_major_version 3;
+	  Sdl.gl_set_attribute Sdl.Gl.context_minor_version 3;
 	  Sdl.delay 5000l;
 	  Sdl.destroy_window w;
 	  Sdl.quit ();

3da093add50d2f9405da51be0c810812e350b4e6
boondaburrah <boondaburrah@ackawi>
2017-02-20 15:23:58 -0500

    minimal sdl example

--- src/main.ml
+++ src/main.ml
@@ -1,2 +1,14 @@
-let () =
-  print_endline "Hello World!"
+open Tsdl
+open Result
+
+let () = match Sdl.init Sdl.Init.video with
+  | Error (`Msg e) -> Sdl.log "Init error: %s" e; exit 1
+  | Ok () ->
+	match Sdl.create_window ~w:640 ~h:480 "SDL OpenGL" Sdl.Window.opengl with
+	| Error (`Msg e) -> Sdl.log "error: %s" e; exit 1
+	| Ok w -> 
+	  Sdl.delay 5000l;
+	  Sdl.destroy_window w;
+	  Sdl.quit ();
+	  exit 0
+
